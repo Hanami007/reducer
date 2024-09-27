315e50add8b4705f52cd2ffbc3cb677859b0ba1d
Hanami007 <bass2545b@gmail.com>
2024-09-27 14:43:44 +0700

    .

--- reducer/src/AddTask.jsx
+++ reducer/src/AddTask.jsx
@@ -1,21 +1,43 @@
-import { useState } from 'react';
+// formReducer.js
 
-export default function AddTask({onAddTask}) {
-  const [text, setText] = useState('');
-  return (
-    <>
-      <input
-        placeholder="Add task"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          setText('');
-          onAddTask(text);
-        }}>
-        Add
-      </button>
-    </>
-  );
+export const initialState = {
+  formData: {
+    firstName: '',
+    lastName: '',
+    company: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
+    agreeToPolicy: false,
+  },
+  userList: [],
+};
+
+export function formReducer(state, action) {
+  switch (action.type) {
+    case 'UPDATE_FORM':
+      return {
+        ...state,
+        formData: {
+          ...state.formData,
+          [action.payload.name]: action.payload.value,
+        },
+      };
+    case 'ADD_USER':
+      return {
+        ...state,
+        userList: [...state.userList, state.formData],
+        formData: initialState.formData, // รีเซ็ตฟอร์ม
+      };
+    case 'TOGGLE_SWITCH':
+      return {
+        ...state,
+        formData: {
+          ...state.formData,
+          agreeToPolicy: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
 }
